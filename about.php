9dbaef397489c98160526256d0e2e59c4d60a5d5
Conor Gilmer <conor.gilmer@gmail.com>
2015-11-18 19:31:25 +0000

    about

---
+++ about.php
@@ -0,0 +1,10 @@
+<?php include ("inc/header.php") ?>
+
+<h1>About</h1>
+<pre>
+This site is just a list of various web stuff, links social media etc. A kind of online desktop.
+</pre>
+<?php include ("inc/footer.php") ?>
+
+
+
---
+++ inc/footer.php
@@ -0,0 +1,22 @@
+
+                </div><!-- .row -->
+                </div><!-- .span12 -->
+              </div><!-- .container -->
+
+              <br/>
+
+            <!-- keep this at the bottom to push down the footer-->
+            <div class="push"><!--//--></div>
+            </div><!-- .wrapper -->
+
+            <!-- footer -->
+            <footer>
+            <div class="container">
+            <div class="row">
+                    <p>&copy; Conor Gilmer 2010-2015</p>
+            </div><!-- .row -->
+            </div><!-- .container -->
+            </footer>
+            
+        </body>
+    </html>
