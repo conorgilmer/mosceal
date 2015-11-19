bc3c9e3a4766eb1fbcf5f25094a049d8cab2bc22
Conor Gilmer <conor.gilmer@gmail.com>
2015-11-19 15:35:05 +0000

    md and yt

--- README.md
+++ README.md
@@ -3,10 +3,26 @@
 This site is just a list of various web stuff, links social media etc. A kind of online desktop.
 And also fiddle about with an API or three
 
-May Also include some of my interests 
+Should Include
+- Twitter
+- Google+
+- Facebook
+- YouTube
+
+May Also include some of my interests and projects I have worked on
 - Chess
 - Radio
-- Politics
+- Politics / Elections
+
+#To Do/ Ideas
+- Add Instagram / Flickr
+- Add Private section with logins
+  - Native
+  - DB
+  - Twitter
+  - Facebook
+  - Google
+  - Yahoo
 
 ## Mise
 Author: http://ie.linkedin.com/in/conorgilmer
---
+++ youtube.php
@@ -0,0 +1,58 @@
+<?php include ("inc/header.php") ?>
+
+<!-- Menu -->
+       <!-- stationary nav bar -->
+            <div class="navbar navbar-fixed-top">
+              <div class="navbar-inner">
+                <div class="container">
+                  <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </a>
+                  <a class="brand" href="index.php">Mo Scéal</a>
+                  <div class="nav-collapse">
+                    <ul class="nav">
+                      <li><a href="index.php">Home</a></li>
+                      <li><a href="twitter.php">Twitter</a></li>
+                      <li><a href="google.php">Google+</a></li>
+                      <li><a href="facebook.php">Facebook</a></li>
+                      <li class="active"><a href="youtube.php">YouTube</a></li>
+		      <li><a href="myCV.php">My CV</a></li>
+                      <li><a href="projects.php">Project</a></li>
+                      <li><a href="about.php">About</a></li>
+                      <li><a href="colophon.php">Colophon</a></li>
+                    </ul>
+                  </div><!--/.nav-collapse -->
+                </div>
+              </div>
+            </div>
+
+           <!-- main page content -->
+          <div class="wrapper">
+          <!-- above the containers to push down the footer -->
+
+              <div class="container">
+
+                <div class="row">
+
+                <div class="span12 helpDoc">
+
+<!-- Page stuff -->
+
+
+<h1> My YouTube Videos</h1>
+<p>Some videos I posted on YouTube.</p>
+ <div class="row text-center">
+
+<iframe id="ytplayer" type="text/html" width="640" height="360"
+src="https://www.youtube.com/embed/?listType=user_uploads&list=cjgilmer"
+frameborder="0" allowfullscreen>
+
+      </div><!-- /.row -->
+
+
+<?php include ("inc/footer.php") ?>
+
+
+
