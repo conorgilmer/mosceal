b87f2aa5994c59bc879ccf4a2e84db05b9791da3
Conor Gilmer <conor.gilmer@gmail.com>
2015-11-20 16:29:46 +0000

    buttons

--- twitter.php
+++ twitter.php
@@ -42,6 +42,27 @@
 	<!-- Page stuff -->
 
 	<h1>Twitter Stuff</h1>
+
+
+<h3>Twitter Buttons</h3>
+			<div class="span3">
+<a href="https://twitter.com/conorgilmer" class="twitter-follow-button" data-show-count="false">Follow @conorgilmer</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+			</div>
+			<div class="span3">
+<a href="https://twitter.com/intent/tweet?screen_name=conorgilmer" class="twitter-mention-button" data-related="conorgilmer">Tweet to @conorgilmer</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+			</div>
+			<div class="span3">
+<a href="https://twitter.com/intent/tweet?button_hashtag=irishchess" class="twitter-hashtag-button" data-related="conorgilmer">Tweet #irishchess</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+			</div>
+			<div class="span3">
+<a href="https://twitter.com/intent/tweet?screen_name=conorgilmer" class="twitter-mention-button" data-related="conorgilmer">Tweet to @conorgilmer</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+			</div>
+			</div>
+			<div class="row">
 			<div class="span4">
               <h3>My Tweets</h3>
               <a class="twitter-timeline" href="https://twitter.com/conorgilmer" data-widget-id="393732727855259648">Tweets by @conorgilmer</a>
@@ -56,6 +77,9 @@
 
          </div> 
 			<div class="span4">
+  <h3>Chess Club Tweets</h3>
+<a class="twitter-timeline" href="https://twitter.com/phibsborochess" data-widget-id="667734738489221120">Tweets by @phibsborochess</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
          </div> 
 <?php include ("inc/footer.php") ?>
