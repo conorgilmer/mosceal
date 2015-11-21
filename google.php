a25a0ef0f83b18ffb044d5ca27097104d1c5c823
Conor Gilmer <conor.gilmer@gmail.com>
2015-11-21 11:36:11 +0000

    4

--- google.php
+++ google.php
@@ -41,18 +41,22 @@
 			<div class="span12 helpDoc">
 
 	<!-- Page stuff -->
-
+<div class="row">
 	<h1>Google+ Stuff</h1>
+<div class="span4">
 
 <h3>Google Badge</h3>
 <script src="https://apis.google.com/js/platform.js" async defer></script>
 <g:person href="https://plus.google.com/u/0/+ConorGilmer" data-rel="author"></g:person>
-
+</div>
+<div class="span4">
 <h3>Google Like</h3>
 
 	<div class="g-plusone" data-annotation="inline" data-width="300"></div>
 
 
+</div>
+</div>
 <?php include ("inc/footer.php") ?>
 
 
