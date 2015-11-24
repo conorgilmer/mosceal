5eed05acf57768a30cd19124e29dd58f33340524
Conor Gilmer <conor.gilmer@gmail.com>
2015-11-24 10:29:01 +0000

    fix typo

--- youtube.php
+++ youtube.php
@@ -49,7 +49,7 @@
 
 <iframe id="ytplayer" type="text/html" width="640" height="360"
 src="https://www.youtube.com/embed/?listType=user_uploads&list=cjgilmer"
-frameborder="0" allowfullscreen>
+frameborder="0" allowfullscreen></iframe>
 
       </div><!-- /.row -->
 
