47bfacc2e321f4df5b95d46ef4e3336968a7ac6c
Conor Gilmer <conor.gilmer@gmail.com>
2015-11-26 21:10:55 +0000

    generic menus

--- twitter.php
+++ twitter.php
@@ -1,34 +1,7 @@
-<?php include ("inc/header.php") ?>
-
-<!-- Menu -->
-       <!-- stationary nav bar -->
-            <div class="navbar navbar-fixed-top">
-              <div class="navbar-inner">
-                <div class="container">
-                  <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                  </a>
-                  <a class="brand" href="index.php">Mo Scéal</a>
-                  <div class="nav-collapse">
-                    <ul class="nav">
-                      <li><a href="index.php">Home</a></li>
-                      <li class="active"><a href="twitter.php">Twitter</a></li>
-                      <li><a href="google.php">Google+</a></li>
-                      <li><a href="facebook.php">Facebook</a></li>
-                      <li><a href="youtube.php">YouTube</a></li>
-                      <li><a href="instagram.php">Instagram</a></li>
-                      <li><a href="flickr.php">Flickr</a></li>
-                      <li><a href="myCV.php">My CV</a></li>
-                      <li><a href="projects.php">Projects</a></li>
-                      <li><a href="about.php">About</a></li>
-                      <li><a href="colophon.php">Colophon</a></li>
-                    </ul>
-                  </div><!--/.nav-collapse -->
-                </div>
-		      </div>
-		    </div>
+<?php include ("inc/header.php");
+$thisPage = "twitter";
+include ("inc/menu.php");
+?>
 
 		   <!-- main page content -->
 		  <div class="wrapper">
