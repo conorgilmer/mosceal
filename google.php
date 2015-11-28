c8422fcc0a5db46e09a5496d8718032045ab9828
Conor Gilmer <conor.gilmer@gmail.com>
2015-11-28 17:50:43 +0000

    gshare

--- google.php
+++ google.php
@@ -17,29 +17,34 @@ include ("inc/menu.php");
 	<!-- Page stuff -->
 <div class="row">
 	<h1>Google+ Stuff</h1>
-<div class="span4">
 
-<h3>Google Badge</h3>
-<script src="https://apis.google.com/js/platform.js" async defer></script>
-<g:person href="https://plus.google.com/u/0/+ConorGilmer" data-rel="author"></g:person>
+<div class="span6">
+<h3>Embedded Post</h3>
+	<!-- Place this tag in your head or just before your close body tag. -->
+	<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+
+	<!-- Place this tag where you want the widget to render. -->
+	<div class="g-post" data-href="https://plus.google.com/+EuropeanCommission/posts/B7PRBkxb5rz"></div>
 </div>
-<div class="span4">
+
+<div class="spanr6">
+<h3>Google Badge</h3>
+	<script src="https://apis.google.com/js/platform.js" async defer></script>
+	<g:person href="https://plus.google.com/u/0/+ConorGilmer" data-rel="author"></g:person>
+
+
 <h3>Google Like</h3>
 
-	<div class="g-plusone" data-annotation="inline" data-width="300"></div>
+        <div class="g-plusone" data-annotation="inline" data-width="300"></div>
 
+<h3>Google Share</h3>
+
+
+    <g:plus action="share"></g:plus>
 
 </div>
 
 
-<div class="span4">
-<h3>Embedded Post</h3>
-<!-- Place this tag in your head or just before your close body tag. -->
-<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
-
-<!-- Place this tag where you want the widget to render. -->
-<div class="g-post" data-href="https://plus.google.com/+EuropeanCommission/posts/B7PRBkxb5rz"></div>
-</div>
 </div>
 <?php include ("inc/footer.php") ?>
 
