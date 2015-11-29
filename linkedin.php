2209ed3aa4c997a2f1b4233aa1f26bc1f30bc31f
Conor Gilmer <conor.gilmer@gmail.com>
2015-11-29 11:18:54 +0000

    linkedin

---
+++ linkedin.php
@@ -0,0 +1,31 @@
+<?php 
+$thisPage = "LinkedIn";
+include ("inc/header.php"); 
+include ("inc/menu.php");
+?>
+
+		   <!-- main page content -->
+
+		  <div class="wrapper">
+		  <!-- above the containers to push down the footer -->
+
+		      <div class="container">
+
+			<div class="row">
+
+
+	<!-- Page stuff -->
+
+	<h1>LinkedIn Stuff</h1>
+<p>My LinkedIn page is <a target="_blank" href="http://www.linkedin.com/in/conorgilmer">http://www.linkedin.com/in/conorgilmer</a></p>
+<div class="span12">
+<h2>Buttons</h2>
+<script src="//platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script>
+<script type="IN/Share" data-url="http://www.conorgilmer.com/mosceal" data-counter="top"></script>
+
+</div>
+
+<?php include ("inc/footer.php") ?>
+
+
+
