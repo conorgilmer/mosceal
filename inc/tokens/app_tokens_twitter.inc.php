419db762230d24d30968aff4cc319f7cda8e77a8
Conor Gilmer <conor.gilmer@gmail.com>
2015-11-30 13:58:23 +0000

    app tokens

---
+++ inc/tokens/app_tokens_twitter.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+/* ****************************************
+ * Author: Conor Gilmer
+ * Date: 
+ *
+ * Ref: www.140dev.com
+ *
+ * Put your tokens in as follows
+ *
+ 	$consumer_key = 'CONSUMER KEY';
+	$consumer_secret = 'CONSUMER SECRET';
+	$user_token = 'USER TOKEN';
+	$user_secret = 'USER SECRET';
+
+ * *************************************** */
+
+
+$consumer_key = '';
+$consumer_secret = '';
+$user_token = '';
+$user_secret = '';
