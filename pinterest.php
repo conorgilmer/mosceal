6f1999e6d90c513f120c2b56ac8a495a8193000c
Conor Gilmer <conor.gilmer@gmail.com>
2015-12-01 15:51:35 +0000

    pinterest page

---
+++ pinterest.php
@@ -0,0 +1,36 @@
+<?php 
+$thisPage = "Pinterest";
+include ("inc/header.php");
+include ("inc/menu.php");
+
+?>
+
+
+<!-- main page content -->
+          <div class="wrapper">
+          <!-- above the containers to push down the footer -->
+
+              <div class="container">
+
+                <div class="row">
+
+                <div class="span12 helpDoc">
+
+<!-- Page stuff -->
+
+
+<h1> My Pinterest Widget</h1>
+<p>Pinterest stuff.</p>
+ <div class="row text-center">
+
+
+     <a data-pin-do="embedUser" href="https://www.pinterest.com/conorgilmer/"data-pin-scale-width="80" data-pin-scale-height="200" data-pin-board-width="400">    Visit Conor's profile on Pinterest.</a><!-- Please call pinit.js only once per page --><script type="text/javascript" async src="//assets.pinterest.com/js/pinit.js"></script>    
+
+
+      </div><!-- /.row -->
+
+
+<?php include ("inc/footer.php") ?>
+
+
+
