a4766ca0449b6c51b626d942c137e844e7748457
Conor Gilmer <conor.gilmer@gmail.com>
2015-12-10 15:33:59 +0000

    shell tokens

---
+++ inc/tokens/loginza_tokens.php
@@ -0,0 +1,13 @@
+<?php
+
+/* ****************************************
+ * Author: Conor Gilmer
+ *
+ * tokens to use loginza authentication
+ *
+ * *************************************** */
+
+$client_id ="";
+$client_secret ="";
+$home_url ="";
+$redirect_url ="";
