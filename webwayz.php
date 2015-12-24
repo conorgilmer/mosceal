267bf0e5df9ae25c0b605aec1fa79439a72f35b7
Conor Gilmer <conor.gilmer@gmail.com>
2015-12-24 18:10:14 +0000

    ww sm1

---
+++ webwayz.php
@@ -0,0 +1,32 @@
+<?php 
+$thisPage = "Webwayz";
+include ("inc/header.php");
+include ("inc/menu.php");
+?>
+
+		   <!-- main page content -->
+		  <div class="wrapper">
+		  <!-- above the containers to push down the footer -->
+
+		      <div class="container">
+
+			<div class="row">
+
+
+	<!-- Page stuff -->
+
+	<h1>Webwayz Social Media</h1>
+
+
+<h3>Twitter and Facebook</h3>
+			<div class="span6">
+<a class="twitter-timeline" href="https://twitter.com/webwayz" data-widget-id="680086568187654144">Tweets by @webwayz</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>			</div>
+			<div class="span6">
+			</div>
+			</div>
+
+         </div> 
+<?php include ("inc/footer.php") ?>
+
+
+
