9d62fc400202ac3ecc6525bfffe0596fa54bf585
Conor Gilmer <conor.gilmer@gmail.com>
2015-12-25 23:42:02 +0000

    pcc

---
+++ phibsborocc.php
@@ -0,0 +1,51 @@
+<?php 
+$thisPage = "Chess";
+include ("inc/header.php");
+include ("inc/menu.php");
+?>
+
+		   <!-- main page content -->
+		  <div class="wrapper">
+		  <!-- above the containers to push down the footer -->
+
+		      <div class="container">
+
+			<div class="row">
+
+
+	<!-- Page stuff -->
+
+	<h1>Phibsboro Chess Club</h1>
+
+
+<h3>Buttons</h3>
+			<div class="span3">
+			</div>
+			<div class="span3">
+			</div>
+			<div class="span3">
+			</div>
+			<div class="span3">
+			</div>
+			</div>
+			<div class="row">
+			<div class="span4">
+              <h3>Tweets</h3>
+         </div> 
+			<div class="span4">
+  <h3>#irishchess</h3>
+<p><a class="twitter-timeline" href="https://twitter.com/hashtag/irishchess" data-widget-id="667173465485635585">#irishchess Tweets</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+
+         </div> 
+			<div class="span4">
+  <h3>Chess Club Tweets</h3>
+<a class="twitter-timeline" href="https://twitter.com/phibsborochess" data-widget-id="667734738489221120">Tweets by @phibsborochess</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+         </div> 
+<?php include ("inc/footer.php") ?>
+
+
+
