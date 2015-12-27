17e689a99a56d538f0b5548c83fd8bae7f5139f5
Conor Gilmer <conor.gilmer@gmail.com>
2015-12-27 11:01:05 +0000

    ww fb btn

--- webwayz.php
+++ webwayz.php
@@ -4,6 +4,15 @@ include ("inc/header.php");
 include ("inc/menu.php");
 ?>
 
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=607946642599752";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 		   <!-- main page content -->
 		  <div class="wrapper">
 		  <!-- above the containers to push down the footer -->
@@ -14,19 +23,29 @@ include ("inc/menu.php");
 
 			<div class="span3">
 <a href="https://twitter.com/webwayz" class="twitter-follow-button" data-show-count="false">Follow @webwayz</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
+
+<div class="fb-follow" data-href="https://www.facebook.com/webwayz" data-layout="standard" data-show-faces="true"></div>
+
 			</div>
 
 			<div class="span3">
 <a href="https://twitter.com/intent/tweet?screen_name=webwayz" class="twitter-mention-button" data-related="webwayz">Tweet to @webwayz</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+<br/>
+<div class="fb-share-button" data-href="http://www.facebook.com/webwayz" data-layout="button_count"></div>
 			</div>
 
 			<div class="span3">
 
 <a href="https://twitter.com/intent/tweet?screen_name=webwayz" class="twitter-mention-button" data-related="webwayz">Tweet to @webwayz</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+<br/>
+<div class="fb-send" data-href="http://www.facebook.com/webwayz"></div>
 			</div>
 
 			<div class="span3">
 <a href="https://twitter.com/share" class="twitter-share-button"{count} data-url="http://www.webwayz.com" data-via="webwayz">Tweet</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
+<div class="fb-like" data-href="http://www.facebook.com/webwayz" data-layout="standard" data-action="like" data-show-faces="true" data-share="false"></div>
 
 			</div>
 
