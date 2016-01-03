2321eccefb76dbf6c0946c7a3dce0c74055cf582
Conor Gilmer <conor.gilmer@gmail.com>
2016-01-03 21:01:25 +0000

    auto year

--- inc/footer.php
+++ inc/footer.php
@@ -10,10 +10,13 @@
             </div><!-- .wrapper -->
 
             <!-- footer -->
+<?php
+$date_made = date("Y");
+?>
             <footer>
             <div class="container">
             <div class="row">
-                    <p>&copy; <a target="_blank" href="mailto:conor.gilmer@gmail.com">Conor Gilmer</a> 2010-2015</p>
+                    <p>&copy; <a target="_blank" href="mailto:conor.gilmer@gmail.com">Conor Gilmer</a> 2010-<?php echo$date_made; ?></p>
             </div><!-- .row -->
             </div><!-- .container -->
             </footer>
--- inc/private/footer.php
+++ inc/private/footer.php
@@ -9,11 +9,13 @@
             <div class="push"><!--//--></div>
             </div><!-- .wrapper -->
 
-            <!-- footer -->
+<?php
+$date_made = date("Y");
+?>
             <footer>
             <div class="container">
             <div class="row">
-                    <p>&copy; <a target="_blank" href="mailto:conor.gilmer@gmail.com">Conor Gilmer</a> 2010-2015</p>
+                    <p>&copy; <a target="_blank" href="mailto:conor.gilmer@gmail.com">Conor Gilmer</a> 2010-<?php echo$date_made; ?></p>
             </div><!-- .row -->
             </div><!-- .container -->
             </footer>
