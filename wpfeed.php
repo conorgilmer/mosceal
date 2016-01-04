51ac5f27669406dee69992e5c46a0116a2e20a43
Conor Gilmer <conor.gilmer@gmail.com>
2016-01-04 22:02:34 +0000

    wpfeed

---
+++ wpfeed.php
@@ -0,0 +1,24 @@
+<?php
+ 
+function getFeed($feed_url) {
+     
+    $content = file_get_contents($feed_url);
+    $x = new SimpleXmlElement($content);
+     
+    echo "<ul>";
+     
+    foreach($x->channel->item as $entry) {
+        echo "<li><a href='$entry->link' title='$entry->title'>" . $entry->title . "</a></li>";
+    }
+    echo "</ul>";
+}
+
+
+
+echo getFeed('http://www.webwayz.com/feed');
+
+?>
+
+
+
+
