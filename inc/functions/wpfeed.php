7c6ba3f31982bc97834bcec95573322faee12539
Conor Gilmer <conor.gilmer@gmail.com>
2016-01-06 13:13:10 +0000

    make tumblr and wp feed consistent

--- inc/functions/wpfeed.php
+++ inc/functions/wpfeed.php
@@ -8,13 +8,21 @@ function getFeed($feed_url) {
     $content = file_get_contents($feed_url);
     $x = new SimpleXmlElement($content);
      
-    echo "<ul>";
+  //  echo "<ul>";
      
     foreach($x->channel->item as $entry) {
         $small_post = substr($entry->description,0,120);
-        echo "<li><a href='$entry->link' title='$entry->title'>" . $entry->title . "</a> " .$small_post . "</li>";
+//        echo "<li><a href='$entry->link' title='$entry->title'>" . $entry->title . "</a> " .$small_post . "</li>";
+
+        $tstamp = $entry->pubDate;
+	$title  = $entry->title;
+	$link   = $entry->link;
+        echo $tstamp.'<p><h3><i>'.$title.'</i></h3>';
+        echo ''.$small_post.'... ';
+        echo "<a target=frame2 href='".$link."'>Read More</a><hr> </p>";
+
     }
-    echo "</ul>";
+//    echo "</ul>";
 }
 
 ?>
