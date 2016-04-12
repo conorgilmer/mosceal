8f85c15a6e3d60689c8dd12ac5195e2c648b0543
Conor Gilmer <conor.gilmer@gmail.com>
2016-04-12 01:42:13 +0100

    ello

---
+++ ello.php
@@ -0,0 +1,46 @@
+<?php 
+$thisPage = "Ello";
+include ("inc/header.php");
+include ("inc/menu.php");
+
+?>
+
+
+<!-- main page content -->
+          <div class="wrapper">
+          <!-- above the containers to push down the footer -->
+
+              <div class="container">
+
+                <div class="row">
+
+                <div class="span12 helpDoc">
+
+<!-- Page stuff -->
+
+
+<h1> My Ello Widget</h1>
+<p>Ello stuff.</p>
+ <div class="row text-center">
+
+
+ 
+        <div class="span4">
+
+<iframe class='button' frameborder='0' border='0' scrolling='no' allowtransparency='true' height='40' width='40' src='https://button.ello.co/button?color=black&username=conorgilmer&size=large'></iframe>
+       </div>
+
+
+        <div class="span4">
+
+
+        <div class="span4">
+        </div>
+       </div>
+      </div><!-- /.row -->
+
+
+<?php include ("inc/footer.php") ?>
+
+
+
