881f6158f3bb9511dc767f1a7d587a5fe9afb6d2
Conor Gilmer <conor.gilmer@gmail.com>
2016-08-31 09:26:17 +0100

    added

--- colophon.php
+++ colophon.php
@@ -5,6 +5,9 @@ include ("inc/header.php");
 
 if(!isset($_SESSION['google_data'])){
 	include ("inc/menu.php");}
+else if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == 1) {
+	die($_SESSION['loggedIn'] . " logined in in ");
+	include ("inc/private/menu.php");}
 else {
 	include ("inc/private/menu.php");}
 ?>
@@ -33,7 +36,7 @@ else {
 <p> Well these pages need to acknowledge some technologies and people who inspired them. Naturally technologies like PHP, HTML, CSS, Bootstrap, API's and widgets from google+, facebook, youtube, instragram, flickr, twitter, tumblr, pinterest and foursquare. Also the lecture notes of Conor O'Reilly and Daniel Griffin.
 </p>
 <p>
-Also some old friends such as the vim(<a target="_blank" href="www.vim.org">www.vim.org</a>) editor and gimp(<a target="_blank" href="www.gimp.org">www.gimp.org</a>) were used to edit code and images.
+Also some old friends such as the vim(<a target="_blank" href="www.vim.org">www.vim.org</a>) editor and gimp(<a target="_blank" href="www.gimp.org">www.gimp.org</a>) were used to edit code and images, a useful online tool I like is Pixlr(<a target="_blank" href="www.pixlr.com">www.pixlr.com</a>).
 </p>
 <p>Developed locally on Linux(Ubuntu) with Apache, MySql and PHP, also then tweaked on XAMPP(<a target="_blank" href="http://www.xampp.org">www.xampp.org</a>) and MAMP (<a target="_blank" href="http://www.mamp.info">www.mamp.info<a>).
 </p>
