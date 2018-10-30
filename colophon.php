981814bc7bf6344f9348500952e4ba881be423cd
Conor Gilmer <conor.gilmer@gmail.com>
2018-10-30 12:20:15 +0000

    update colophon since move

--- colophon.php
+++ colophon.php
@@ -33,11 +33,12 @@ else {
 <!--h1>Colophon</h1-->
 
 
-<p> Well these pages need to acknowledge some technologies and people who inspired them. Naturally technologies like PHP, HTML, CSS, Bootstrap, API's and widgets from google+, facebook, youtube, instragram, flickr, twitter, tumblr, pinterest and foursquare. Also the lecture notes of Conor O'Reilly and Daniel Griffin.
+<p> Well these pages need to acknowledge some technologies and people who inspired them. Naturally technologies like PHP, HTML, CSS, Bootstrap, API's and widgets from google+, facebook, youtube, instragram, flickr, twitter, tumblr, pinterest and foursquare. Also the lecture notes of Conor O'Reilly and Daniel Griffin, as well as evolving from some ideas while working with Fintan Killoran.
 </p>
 <p>
 Also some old friends such as the vim(<a target="_blank" href="http://www.vim.org">www.vim.org</a>) editor and gimp(<a target="_blank" href="http://www.gimp.org">www.gimp.org</a>) were used to edit code and images, a useful online tool I like is Pixlr(<a target="_blank" href="http://www.pixlr.com">www.pixlr.com</a>).
 </p>
+<p>I have used both Eclipse (www.eclipse.org) and NetBeans(www.netbeans.org) while developing some of the components in this and which it links to.</p> 
 <p>Developed locally on Linux(Ubuntu) with Apache, MySql and PHP, also then tweaked on XAMPP(<a target="_blank" href="http://www.xampp.org">www.xampp.org</a>) and MAMP (<a target="_blank" href="http://www.mamp.info">www.mamp.info<a>).
 </p>
 <?php include ("inc/footer.php") ?>
