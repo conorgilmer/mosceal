4841f92b54b9e519a765fd0927557d45549f2222
Conor Gilmer <conor.gilmer@gmail.com>
2018-10-30 13:16:08 +0000

    update menu

--- inc/menu.php
+++ inc/menu.php
@@ -15,15 +15,13 @@
                     <ul class="nav">
                       <li<?php if ($thisPage=="Home") echo " class=\"active\"";?> ><a href="index.php">Home</a></li>
 
-
+ <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Social Media
+          <span class="caret"></span></a>
+          <ul class="dropdown-menu">
                       <li<?php if ($thisPage=="Twitter") echo " class=\"active\"";?>><a href="twitter.php">Twitter</a></li>
                       <li<?php if ($thisPage=="Google") echo " class=\"active\"";?>><a href="google.php">Google+</a></li>
                       <li<?php if ($thisPage=="Facebook") echo " class=\"active\"";?>><a href="facebook.php">Facebook</a></li>
-
- <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Others
-          <span class="caret"></span></a>
-          <ul class="dropdown-menu">
                       <li<?php if ($thisPage=="YouTube") echo " class=\"active\"";?>><a href="youtube.php">YouTube</a></li>
                       <li<?php if ($thisPage=="Instagram") echo " class=\"active\"";?>><a href="instagram.php">Instagram</a></li>
                       <li<?php if ($thisPage=="Flickr") echo " class=\"active\"";?>><a href="flickr.php">Flickr</a></li>
