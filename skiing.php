8dc2e013a773a6d93520c8823ef9df9cee7abe7a
Conor Gilmer <conor.gilmer@gmail.com>
2018-11-14 19:43:33 +0000

    update going

--- skiing.php
+++ skiing.php
@@ -54,9 +54,17 @@ else {
 <p>Navan Travel <a target="_blank" href="https://www.travalue.ie/ski">www.travalue.ie/ski</a></p>
 <p>Snow Forecast <a target="_blank" href="https://www.snow-forecast.com/">www.snow-forecast.com</a></p>
 <p>On the Snow <a target="_blank" href="https://www.onthesnow.com/">www.onthesnow.com</a></p>
-<p>Holiday Shuttle <a target="_blank" href="https://www.holiday-shuttle.at/">https://www.holiday-shuttle.at</a></p>
-<p>Salzburg Tours & Taxis <a target="_blank" href="https://www.salzburgtours.at/">www.salzburgtours.at</a></p>
-<p>Salzburg Limousine Service <a target="_blank" href="https://salzburglimousineservice.com/">salzburglimousineservice.com</a></p>
+
+<h2>Airlines from Dublin</h2>
+<p>Airlines from dublin to airports to ski in Austria or Germany.
+
+<p> <a target="_blank" href="https://www.aerlingus.com/">Aer Lingus</a> fly from Dublin to Munich, Salzburg and Vienna.</p>
+<p> <a target="_blank" href="https://www.ryanair.com/">RyanAir</a> fly from Dublin to Munich, Salzburg, Memmingham, Bergamo(Italy) and Bratislava(Slovakia).</p>
+
+<p> <a target="_blank" href="https://www.laudamotion.com/">Lauda Motion</a> fly from Dublin to Innsbruck and Vienna. The use the Ryanair website</p>
+
+<p> <a target="_blank" href="https://www.luftansa.com/">Luftansa</a> fly from Dublin to Munich.</p>
+ </p>
 
 <h2>Tips</h2>
 <p>One thing I recommend is to check when the school mid terms breaks are, normally it increases the prices for flights, accommodation, and availablilty. Once or twice we had extra traffic on the slopes and lifts since 1/2 of Holland, seemed to be skiing the same week as us.</p>
