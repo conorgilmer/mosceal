19bf2f8675729c93ce855b37617364aade916cee
Conor Gilmer <conor.gilmer@gmail.com>
2018-12-07 18:20:56 +0000

    log of OOC

---
+++ myOOC.php
@@ -0,0 +1,56 @@
+<?php
+$thisPage = "OOC";
+include ("inc/header.php");
+include ("inc/menu.php");
+?>
+
+
+<!-- Page stuff -->
+<!-- main page content -->
+            <div class="wrapper">
+            <!-- the wrapper is above the containers to push down the footer -->
+
+             <div class="container">
+
+
+                <div class="row ">
+                       <div class="span12 docpanel5">
+                            <h2 class="whitefont"> Certificates </h2>
+                       </div> <!-- .span12 -->
+                  </div><!-- .row -->
+
+                  <div class="row ">
+                       <div class="span12">
+			 <h2>Google </h2>
+                            <p class="padFirstP">Google Analytics Individual Qualification (Google) </p>
+                            <p class="padFirstP">Google Adwords Certified - Mobile, Display, Search, Video and Shopping (Google) </p>
+                            <p class="padFirstP">Mobile Sites Certification (Google) </p>
+			 <h2>Microsoft </h2>
+                            <p class="padFirstP">Bing Ads Accredited Professional (Microsoft) </p>
+			 <h2>HubSpot </h2>
+                            <p class="padFirstP">Inbound Certification (HubSpot) </p>
+                            <p class="padFirstP">Email Marketing Certification (HubSpot) </p>
+                            <p class="padFirstP">Content Marketing Certification (HubSpot) </p>
+			 <h2>Google Digital Garage / IAB / Open University </h2>
+                            <p class="padFirstP">Certificate in Online Proficiency (Google/Digital Garage) </p>
+                            <p class="padFirstP">Fundamentals in Digital Marketing Certification (Google/Digital Garage/IAB/Open University) </p>
+			 <h2>LinkedIn Learning</h2>
+                            <p class="padFirstP">WordPress 5.0 Essentials (LinkedIn Learning) </p>
+			 <h2>Twitter</h2>
+                            <p class="padFirstP">Twitter Flight School </p>
+			 <h2>Oracle</h2>
+                            <p class="padFirstP">Java Coding and Concepts</p>
+                       </div> <!-- .span12 -->
+                  </div><!-- .row -->
+
+                <br/>
+
+             </div><!-- .container -->
+
+<!-- Footer -->
+
+
+<?php include ("inc/footer.php") ?>
+
+
+
