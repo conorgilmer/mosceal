09fa18ed136a8e6b290f5a05f372c818f30a7daa
Conor Gilmer <conor.gilmer@gmail.com>
2021-09-30 08:36:20 +0100

    adding new page

---
+++ mscstuff.php
@@ -0,0 +1,36 @@
+<?php 
+$thisPage = "MSc Stuff";
+include ("inc/header.php");
+include ("inc/menu.php");
+?>
+
+<!-- main page content -->
+          <div class="wrapper">
+          <!-- above the containers to push down the footer -->
+
+              <div class="container">
+
+                <div class="row">
+
+                <div class="span12 helpDoc">
+
+<!-- Page stuff -->
+
+
+<h1>MSc Stuff</h1>
+<p></p>
+<h2>Tools</h2>
+<p>
+Some tools I have used during my Masters which I find useful
+<ul>
+<li><a target="_blank" href="https://thonny.org/">Thonny</a> - Python IDE</li>
+<li><a target="_blank" href="https://www.anaconda.com/products/individual">Anaconda</a> "Data Science Toolkit" - Python Jupyter Notebook etc.</li>
+<li><a target="_blank" href="https://online.visual-paradigm.com/">Visual Paradigm</a> Online Drawing Class Diagrams and Flowcharts etc.</li>
+<li><a target="_blank" href="https://app.diagrams.net/">Diagrams.net/Draw.io</a> Online Drawing Class Diagrams etc.</li>
+<li><a target="_blank" href="https://www.citethisforme.com/</a> Online generate citations (IEEE format).</li>
+</ul>
+</p>
+<?php include ("inc/footer.php") ?>
+
+
+
