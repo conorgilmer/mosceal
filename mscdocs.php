5b10bbcb5cfc86a1276ce0280ac676983e958215
Conor Gilmer <conor.gilmer@gmail.com>
2021-11-06 12:51:51 +0000

    my essays

---
+++ mscdocs.php
@@ -0,0 +1,36 @@
+<?php 
+$thisPage = "MSc Stuff";
+include ("inc/header.php");
+include ("inc/menu.php");
+?>
+
+<!-- main page content -->
+          <div class="wrapper">
+          <!-- above the containers to push down the footer -->
+
+              <div class="container">
+
+                <div class="row">
+
+                <div class="span12 helpDoc">
+
+<!-- Page stuff -->
+
+
+<h1>MSc Stuff</h1>
+
+<h2>Essays & Articles</h2>
+<p>
+Here are some papers I have written...
+<ul>
+<li><a target="_blank" href="docs/thetechnologicalsingularity.pdf">The Technological Singularity.</a></li>
+<li><a target="_blank" href="docs/TheEndtoPrivacyCG.pdf">The End to Privacy.</a></li>
+<li><a target="_blank" href="docs/RobotsinSociety.pdf">Advantages and Disadvantages of Robots in Society.</a></li>
+</ul>
+<h2>Tools</h2>
+</p>
+
+<?php include ("inc/footer.php") ?>
+
+
+
