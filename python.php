dcd4fe9b2a6fb6aab81f26d3fe8a9a30227805e6
Conor Gilmer <conor.gilmer@gmail.com>
2023-02-02 14:46:55 +0000

    python projects

--- python.php
+++ python.php
@@ -20,16 +20,25 @@ include ("inc/menu.php");
 <h1>Python</h1>
 
 
-<h2>Python Links</h2><i>(To Do)</i>
+<h2>Python Links</h2>
 <p>Some useful python tools and links.
 <ul>
 <li><a href="https://thonny.org/">Thonny - Python Editor for Beginners</a> </li>
 <li><a href="https://www.anaconda.com/products/distribution">Anaconda - Data Science Environment</a> </li>
+<li><a href="https://www.shanelynn.ie/bar-plots-in-python-using-pandas-dataframes/">Shane Lee - excellent examples of Python using Pandas for graphs</a> </li>
 </ul>
 </p>
 
 <h2>Python Projects</h2>
+<i>(To Do)</i>
 <p> To Do use python to draw pie chart, bar chart and line charts
+<ul>
+<li>Read from CSV/Excel file draw bar chart</li>
+<li>Read from CSV/Excel file draw line chart</li>
+<li>Read from CSV/Excel file draw pie chart</li>
+<li>Read from CSV/Excel file draw bar (Gantt) chart</li>
+</ul>
+
 </p>
 
 <?php include ("inc/footer.php") ?>
