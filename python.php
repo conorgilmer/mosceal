66a14f3edc7eaa9a15afe97c7fc71f68a39e06ab
Conor Gilmer <conor.gilmer@gmail.com>
2023-02-02 14:37:57 +0000

    python links

---
+++ python.php
@@ -0,0 +1,38 @@
+<?php 
+$thisPage = "Projects";
+include ("inc/header.php");
+include ("inc/menu.php");
+?>
+
+<!-- main page content -->
+          <div class="wrapper">
+          <!-- above the containers to push down the footer -->
+
+              <div class="container">
+
+                <div class="row">
+
+                <div class="span12 helpDoc">
+
+<!-- Page stuff -->
+
+
+<h1>Python</h1>
+
+
+<h2>Python Links</h2><i>(To Do)</i>
+<p>Some useful python tools and links.
+<ul>
+<li><a href="https://thonny.org/">Thonny - Python Editor for Beginners</a> </li>
+<li><a href="https://www.anaconda.com/products/distribution">Anaconda - Data Science Environment</a> </li>
+</ul>
+</p>
+
+<h2>Python Projects</h2>
+<p> To Do use python to draw pie chart, bar chart and line charts
+</p>
+
+<?php include ("inc/footer.php") ?>
+
+
+
